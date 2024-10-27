b78e940a5f003172c12fb05d259009ee29d9cd19
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-27 13:39:59 +0300

    hero class is added

---
+++ src/main/java/org/example/Task2/Hero.java
@@ -0,0 +1,13 @@
+package org.example.Task2;
+
+public class Hero {
+    private final String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public void attackEnemy(){
+        System.out.println("Hero is attacking!");
+    }
+}
