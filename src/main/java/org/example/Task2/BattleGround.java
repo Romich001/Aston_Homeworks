3b9919f960e44eacf843c1955a931d4cf3d9c2d3
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-27 16:24:09 +0300

    BattleGround battle is gone

--- src/main/java/org/example/Task2/BattleGround.java
+++ src/main/java/org/example/Task2/BattleGround.java
@@ -1,16 +1,27 @@
 package org.example.Task2;
 
 import org.example.Task2.Enemy.Enemy;
+import org.example.Task2.Heroes.Hero;
 import org.example.Task2.Heroes.Warrior;
 
 public class BattleGround {
+
+    private static final Warrior gorka = new Warrior("Gorka", 15);
+    private static final Enemy artes = new Enemy(15);
+    private static String winner;
+
     public static void main(String[] args) {
-        var gorka = new Warrior("Gorka", 15);
-        var artes = new Enemy(15);
+
         do {
             gorka.attackEnemy(artes);
+            artes.attack(gorka);
+        } while (gorka.isAlive() && artes.isAlive());
 
-        } while (artes.isAlive());
-
+        if (gorka.isAlive()) {
+            winner = gorka.getNAME();
+        } else {
+            winner = artes.getClass().getSimpleName();
+        }
+        System.out.printf("%s is winner!!!\n", winner);
     }
 }
