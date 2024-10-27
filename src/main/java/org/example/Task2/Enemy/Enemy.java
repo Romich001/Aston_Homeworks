00a95b32f184e7996362c8881ca4ff1accc1f5fb
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-27 14:07:05 +0300

    enemy is added

---
+++ src/main/java/org/example/Task2/Enemy/Enemy.java
@@ -0,0 +1,21 @@
+package org.example.Task2.Enemy;
+
+public class Enemy {
+    private int health;
+
+    public Enemy(int health) {
+        this.health = health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+}
