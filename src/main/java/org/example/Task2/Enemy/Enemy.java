d1980641ce602da741af022cef40e489ea19e6c5
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-27 15:23:00 +0300

    Enemy added attack()

--- src/main/java/org/example/Task2/Enemy/Enemy.java
+++ src/main/java/org/example/Task2/Enemy/Enemy.java
@@ -1,12 +1,19 @@
 package org.example.Task2.Enemy;
 
 import org.example.Interfaces.Mortal;
+import org.example.Task2.Heroes.Hero;
 
 public class Enemy implements Mortal {
     private int health;
+    private final int DAMAGE = 4;
 
     public Enemy(int health) {
         this.health = health;
+    }
+
+    public void attack(Hero hero) {
+        System.out.println("Enemy is attacking!");
+        hero.takeDamage(DAMAGE);
     }
 
     public int getHealth() {
