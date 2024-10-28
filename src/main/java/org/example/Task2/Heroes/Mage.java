fab58374bee25bd97c470e15c8173bfa9815d283
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-28 16:26:57 +0300

    Mage is added perk "casting fireball"

--- src/main/java/org/example/Task2/Heroes/Mage.java
+++ src/main/java/org/example/Task2/Heroes/Mage.java
@@ -1,6 +1,7 @@
 package org.example.Task2.Heroes;
 
 import org.example.Task2.Enemy.Enemy;
+import org.example.Task2.Enemy.Pinocchio;
 
 public class Mage extends Hero {
 
@@ -12,8 +13,26 @@ public class Mage extends Hero {
 
     @Override
     public void attackEnemy(Enemy enemy) {
-        System.out.printf("Mage %s is attacking!\n", getNAME());
-        enemy.takeDamage(STRENGTH);
+        var attackClaim = "Mage %s is attacking!\n";
+        if (getHealth() <= STRENGTH) {      //if mage's health is less than it's STRENGTH than mage casts fireball.
+            castFireball(enemy);
+        } else {
+            System.out.printf(attackClaim, getNAME());
+            enemy.takeDamage(STRENGTH);
+        }
+
+    }
+
+    private void castFireball(Enemy enemy) {
+        var damageForPinocchio = 15;       //damage for pinocchio
+        var modifierForAttack = 3;
+        var attackClaim = " is casting fireball!!!!!!!!!";
+        System.out.println(getNAME() + attackClaim);
+        if (enemy instanceof Pinocchio) {            //if enemy is Pinocchio then damage is 15xp. Log is a good fuel.
+            enemy.takeDamage(damageForPinocchio);
+        } else {
+            enemy.takeDamage(STRENGTH * modifierForAttack);   //damage to other types of enemies is STRENGTH multiplied to modifier.
+        }
     }
 
 }
