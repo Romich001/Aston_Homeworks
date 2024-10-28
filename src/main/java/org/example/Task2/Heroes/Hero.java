ba4aab0738ad6c5bd5b894eebc7914bf2b20a6fc
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-28 17:01:40 +0300

    got setHealth()

--- src/main/java/org/example/Task2/Heroes/Hero.java
+++ src/main/java/org/example/Task2/Heroes/Hero.java
@@ -7,6 +7,8 @@ public abstract class Hero implements Mortal {
 
 
     private final String NAME;
+
+
     private int health;
 
     public Hero(String name, int health) {
@@ -30,9 +32,13 @@ public abstract class Hero implements Mortal {
         System.out.printf("%s health is %d\n", this.getClass().getSimpleName(), health);
     }
 
-    abstract void attackEnemy(Enemy enemy);
+    public abstract void attackEnemy(Enemy enemy);
 
     public int getHealth() {
         return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
     }
 }
