707e092b6a42aa747a3bc0ae517204fe15b4d694
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-10-28 16:52:09 +0300

    add claim for attacking to Pinocchio

--- src/main/java/org/example/Task2/Heroes/Mage.java
+++ src/main/java/org/example/Task2/Heroes/Mage.java
@@ -17,7 +17,7 @@ public class Mage extends Hero {
         if (getHealth() <= STRENGTH) {      //if mage's health is less than it's STRENGTH than mage casts fireball.
             castFireball(enemy);
         } else {
-            System.out.printf(attackClaim, getNAME());
+            System.out.printf(attackClaim, getNAME());  //standard attack
             enemy.takeDamage(STRENGTH);
         }
 
@@ -27,9 +27,12 @@ public class Mage extends Hero {
         var damageForPinocchio = 15;       //damage for pinocchio
         var modifierForAttack = 3;
         var attackClaim = " is casting fireball!!!!!!!!!";
+        var claimForPinocchio = "Good wood. Dry firewood";
+
         System.out.println(getNAME() + attackClaim);
         if (enemy instanceof Pinocchio) {            //if enemy is Pinocchio then damage is 15xp. Log is a good fuel.
             enemy.takeDamage(damageForPinocchio);
+            System.out.println(claimForPinocchio);
         } else {
             enemy.takeDamage(STRENGTH * modifierForAttack);   //damage to other types of enemies is STRENGTH multiplied to modifier.
         }
