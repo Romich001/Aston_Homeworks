0e79382d8a54a04a0ab85f49c5f501ad711b2409
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-11-04 20:05:38 +0300

    ListsDemo created

--- src/main/java/task3/ListsDemo.java
+++ src/main/java/task3/ListsDemo.java
@@ -2,6 +2,19 @@ package task3;
 
 public class ListsDemo {
     public static void main(String[] args) {
+        MyLinkedList<Integer> newList = new MyLinkedList<>();
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+        System.out.println(newList);
+        System.out.println("removed: " + newList.remove(1));
+        System.out.println("Size: " + newList.length());
+        System.out.println(newList);
+        System.out.println("removed: " + newList.remove(1));
+        System.out.println("Size: " + newList.length());
+        System.out.println(newList);
+        System.out.println("removed: " + newList.remove(0));
+        System.out.println("Size: " + newList.length());
 
     }
 }
