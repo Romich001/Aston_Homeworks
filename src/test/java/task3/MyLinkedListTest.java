6628c93079f47a2cb4ae2eaa0efcf1a04ebcaf1a
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-11-04 20:04:52 +0300

    added test

---
+++ src/test/java/task3/MyLinkedListTest.java
@@ -0,0 +1,44 @@
+package task3;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+
+    @BeforeAll
+    static void createList() {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+    }
+
+    @Test
+    void length() {
+
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
