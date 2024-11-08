e3c34ad9eb7c2b660843d132975209b8a23b3a5e
Roman Voyttyuk <romavoytyuk@gmail.com>
2024-11-08 18:58:29 +0300

    ListDemo finished

--- src/main/java/task3/ListsDemo.java
+++ src/main/java/task3/ListsDemo.java
@@ -1,20 +1,47 @@
 package task3;
 
+import java.util.List;
+
 public class ListsDemo {
     public static void main(String[] args) {
-        MyLinkedList<Integer> newList = new MyLinkedList<>();
-        newList.add(1);
-        newList.add(2);
-        newList.add(3);
-        System.out.println(newList);
-        System.out.println("removed: " + newList.remove(1));
-        System.out.println("Size: " + newList.length());
-        System.out.println(newList);
-        System.out.println("removed: " + newList.remove(1));
-        System.out.println("Size: " + newList.length());
-        System.out.println(newList);
-        System.out.println("removed: " + newList.remove(0));
-        System.out.println("Size: " + newList.length());
+        sortTest();
+        addAllDemo();
+        removeAllDemo();
+        iterateDemo();
+        System.out.println(MyCollectionSorter.sort(List.of(3, 2, 1)));
+
+
+//        more tests in test.java.task3.tests
+
+    }
+
+    private static void iterateDemo() {
+        var list = new MyLinkedList<>(List.of(1, 2, 3));
+
+        list.forEach(System.out::println);
+    }
+
+    private static void sortTest() {
+        var myList = new MyLinkedList<Integer>();
+        myList.addAll(List.of(4, 3));
+        myList.sort();
+        System.out.println(myList);
+
+    }
+
+    private static void removeAllDemo() {
+        var list = new MyLinkedList<Integer>(List.of(1, 2, 3));
+        System.out.println("List before removing: " + list);
+        list.remove(1);
+        System.out.println("List after removing: " + list);
+
+    }
+
+
+    private static void addAllDemo() {
+        var list = new MyLinkedList<Integer>();
+        list.addAll(List.of(1, 2, 4));
+        System.out.println(list);
 
     }
 }
